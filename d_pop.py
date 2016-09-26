587f4278e2ec2df71a0b2f479bb56e08aa8b4800
Jared <jared.j.lorince@gmail.com>
2016-09-26 11:27:14 -0500

    reference pop

--- d_pop.py
+++ d_pop.py
@@ -42,15 +42,19 @@ def process_year_refs(year):
 
     year_start = time.time()
 
-    metadata = pd.read_table('{}metadata/{}.txt.gz'.format(parsed_dir,year),compression='gzip',header=None,names=['uid','date','pubtype','volume','issue','pages','paper_title','source_title','doctype'],usecols=['uid','date'],parse_dates=['date'])
+    metadata = pd.read_table('{}metadata/{}.txt.gz'.format(parsed_dir,year),compression='gzip',header=None,names=['uid','date','pubtype','volume','issue','pages','paper_title','source_title','doctype'],usecols=['uid','date'],parse_dates=['date'],quoting=3)
+    print 'metadata: {}'.format(len(metadata))
 
     refs = pd.read_table('{}references/{}.txt.gz'.format(parsed_dir,year),compression='gzip',header=None,names=['uid','total_refs','refs','missing_refs'],usecols=['uid','refs'])
     refs['refs'] = refs['refs'].fillna('')
+    print 'refs: {}'.format(len(refs))
 
     cats = pd.concat([pd.read_table('{}subjects/{}.txt.gz'.format(parsed_dir,y),compression='gzip',header=None,names=['uid','heading','subheading','categories'],usecols=['uid','categories']) for y in xrange(1950,year+1)])
     cats['categories'] = cats['categories'].fillna('')
+    print 'cats: {}'.format(len(cats))
 
     merged = refs.merge(metadata,on='uid')
+    print 'refs merged with metadata: {}'.format(len(merged))
 
     rows = []
     for row in merged.itertuples():
@@ -61,8 +65,15 @@ def process_year_refs(year):
             raise(e)
 
     merged = pd.DataFrame(rows,columns=['date','uid']) # NOTE THAT HERE UID IS THE UID OF A REFERENCED PAPER!
+    print 'refs merged with metadata, unpacked: {}'.format(len(merged))    
+
+    merged['cnt'] = 1
+    merged = merged.groupby(['date','uid']).sum().reset_index()
+    print 'reference counts: {}'.format(len(merged))
 
     merged = merged.merge(cats,on='uid')
+    print 'merged with categories: {}'.format(len(merged))
+
 
     rows = []
     for row in merged.itertuples():
@@ -73,8 +84,10 @@ def process_year_refs(year):
             raise(e)
 
     final = pd.DataFrame(rows,columns=['date','ref','category'])
+    print 'merged with categories (unpacked): {}'.format(len(final))
 
-    resampled = merged.groupby(['date','category']).count()
+    resampled = final.groupby(['date','category']).count()
+    print 'resampled: {}'.format(len(resampled))
 
     td = str(datetime.timedelta(seconds=time.time()-year_start))
     records = len(resampled)
@@ -90,7 +103,7 @@ if __name__ == '__main__':
     final_df = pd.concat(pool.map(process_year_refs,years))
     td = str(datetime.timedelta(seconds=time.time()-overall_start))
     print "Parsing complete  in {} (total data length: {})".format(td, len(final_df))
-    final_df.to_pickle('d_pop.pkl')
+    final_df.to_pickle('d_pop_refs.pkl')
 
     pool.close()
 
